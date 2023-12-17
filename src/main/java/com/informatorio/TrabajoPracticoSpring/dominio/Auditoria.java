a16c39c1acd6112374c01acfdd770c88bf9d5fdc
Gerardo-Rioss <gerarios26@gmail.com>
2023-12-17 13:46:17 -0300

    Update Auditoria.java

--- src/main/java/com/informatorio/TrabajoPracticoSpring/dominio/Auditoria.java
+++ src/main/java/com/informatorio/TrabajoPracticoSpring/dominio/Auditoria.java
@@ -1,4 +1,38 @@
 package com.informatorio.TrabajoPracticoSpring.dominio;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "auditoria")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Auditoria {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36,columnDefinition = "varchar(36)",updatable = false,nullable = false)
+    private UUID id;
+
+    @Column(name = "fecha")
+    private LocalDateTime fecha;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private Usuario usuario ;
+
+    @Column(name = "accion")
+    private String accion;
+
 }
