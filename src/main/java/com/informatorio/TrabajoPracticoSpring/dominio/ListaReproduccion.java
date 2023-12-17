644abb7112c8b730f85e7314d9bbf292fb4f4f0d
Gerardo-Rioss <gerarios26@gmail.com>
2023-12-17 13:03:55 -0300

    Creado Clase Usuario

---
+++ src/main/java/com/informatorio/TrabajoPracticoSpring/dominio/ListaReproduccion.java
@@ -0,0 +1,4 @@
+package com.informatorio.TrabajoPracticoSpring.dominio;
+
+public class ListaReproduccion {
+}
---
+++ src/main/java/com/informatorio/TrabajoPracticoSpring/dominio/Usuario.java
@@ -0,0 +1,38 @@
+package com.informatorio.TrabajoPracticoSpring.dominio;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Usuario {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36,columnDefinition = "varchar(36)",updatable = false,nullable = false)
+    private UUID id;
+
+    @Column(name="nombre")
+    private String nombre;
+
+    @Column(name="nombreUsuario")
+    private String nombreUsuario;
+
+    @OneToMany(fetch = FetchType.EAGER,mappedBy = "usuario", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<ListaReproduccion> listaReproduccion = new ArrayList<>();
+
+}
