2e34bf0cdb91a8a99b876fb63e100b11dbfaebe0
Gerardo-Rioss <gerarios26@gmail.com>
2023-12-17 12:23:53 -0300

    Initial commit

---
+++ src/main/java/com/informatorio/TrabajoPracticoSpring/TrabajoPracticoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.informatorio.TrabajoPracticoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoPracticoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoPracticoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/informatorio/TrabajoPracticoSpring/TrabajoPracticoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.TrabajoPracticoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoPracticoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
