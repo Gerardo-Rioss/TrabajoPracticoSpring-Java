e8e645a68b9ff04146a1596c3105626264e7509b
Gerardo-Rioss <gerarios26@gmail.com>
2023-12-17 13:33:39 -0300

    Update Genero.java

--- src/main/java/com/informatorio/TrabajoPracticoSpring/dominio/Genero.java
+++ src/main/java/com/informatorio/TrabajoPracticoSpring/dominio/Genero.java
@@ -1,4 +1,34 @@
 package com.informatorio.TrabajoPracticoSpring.dominio;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "genero")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Genero {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36,columnDefinition = "varchar(36)",updatable = false,nullable = false)
+    private UUID id;
+
+    @Column(name="nombre")
+    private String nombre;
+
+    @OneToMany(fetch = FetchType.EAGER,mappedBy = "genero", cascade = {CascadeType.REMOVE, CascadeType.PERSIST, CascadeType.REFRESH})
+    private List<Cancion> canciones = new ArrayList<>();
 }
